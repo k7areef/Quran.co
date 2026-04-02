41cb611254138bfa4fbbf9433c4137bf638de35b
k7areef <kh3reef@gmail.com>
2026-04-02 14:09:51 +0200

    fix: update ChaptersSidebarModal for mobile responsiveness - Adjusted class names in ChaptersSidebarModal to hide the sidebar on larger screens, improving mobile layout and user experience.

--- src/components/Modals/ChaptersSidebarModal.jsx
+++ src/components/Modals/ChaptersSidebarModal.jsx
@@ -10,7 +10,7 @@ function ChaptersSidebarModal() {
 
     return (createPortal(<div
         onClick={() => setIsChaptersSidebarOpen(false)}
-        className='chapters-sidebar-modal h-dvh w-full fixed top-0 left-0 z-50 bg-black/50 backdrop-blur-sm'
+        className='chapters-sidebar-modal h-dvh w-full fixed lg:hidden top-0 left-0 z-50 bg-black/50 backdrop-blur-sm'
     >
         <Sidebar className='text-white rounded-none flex-col-reverse max-lg:pt-3' onClick={(e) => e.stopPropagation()} />
     </div>, document.getElementById("modal-root")))
